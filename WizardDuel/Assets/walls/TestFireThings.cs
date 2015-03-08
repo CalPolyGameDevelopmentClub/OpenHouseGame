7cda1a5e3b95bd47faa4f79d8bca0956810d9770
Kyle Piddington <kpiddy@gmail.com>
2015-03-07 17:33:34 -0800

    Added bouncy walls

---
+++ WizardDuel/Assets/walls/TestFireThings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFireThings : MonoBehaviour {
+	public GameObject bullet;
+	public float fireX;
+	public float fireY;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			GameObject clone = (GameObject)Instantiate(bullet,transform.position,Quaternion.identity);
+			clone.rigidbody2D.AddForce(new Vector2(fireX,fireY)/Time.fixedDeltaTime);
+		}
+	}
+
+}
---
+++ WizardDuel/Assets/walls/WallScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+
+	public float breakThreshhold = 150f;
+	public float forceThreshhold = 100f;
+	bool isWall = true;
+	// Use this for initialization
+	void Start () {
+		this.rigidbody2D.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+
+		Vector2 force = col.rigidbody.mass * col.rigidbody.velocity / Time.fixedDeltaTime;
+		print (col.rigidbody.velocity);
+		if(force.magnitude > forceThreshhold)
+		{
+			breakThreshhold -= force.magnitude;
+			if (breakThreshhold < 0) {
+				isWall = false;
+				this.rigidbody2D.isKinematic = false;
+				this.rigidbody2D.AddForce(new Vector2(-force.x,force.y));
+				this.rigidbody2D.AddTorque(Random.Range(-25f,25f));
+				this.rigidbody2D.collider2D.isTrigger=true;
+			}
+			else
+			{
+				//col.collider.rigidbody2D.AddForce(-2*force);
+			}
+		}
+	}
+}
