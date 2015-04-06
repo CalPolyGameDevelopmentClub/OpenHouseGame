de4b4b1c9950e8e49b3148f4ebd66e83d06ddd02
Ethan Nakashima <ethanmn@hotmail.com>
2015-04-06 12:57:20 -0700

    Updated movement. Note: Controller is required

--- WizardDuel/Assets/Scripts/TestProjectileExplostion.cs
+++ WizardDuel/Assets/Scripts/TestProjectileExplostion.cs
@@ -11,13 +11,13 @@ public class TestProjectileExplostion : MonoBehaviour {
 	void Start () {
 		GameObject[] playerList = GameObject.FindGameObjectsWithTag("Player");
 		foreach ( GameObject player in playerList ) {
-			float dis = Vector2.Distance(this.rigidbody2D.position, player.rigidbody2D.position);
+			float dis = Vector2.Distance(this.GetComponent<Rigidbody2D>().position, player.GetComponent<Rigidbody2D>().position);
 			float forcePower = dis/this.radius;
 			if (0 < forcePower && forcePower < 1) {
 				float xForce = (Mathf.Pow((1 - dis), 4))*315;
 				float yForce = (Mathf.Pow((1 - dis), 4))*315;
 				
-				player.rigidbody2D.AddForce(new Vector2(xForce, yForce));
+				player.GetComponent<Rigidbody2D>().AddForce(new Vector2(xForce, yForce));
 			}
 		}
 	}
