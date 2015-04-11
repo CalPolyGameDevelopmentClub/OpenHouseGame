0e49d1838bcf5baaf9986796412794dd6c4a1238
Kyle Piddington <kpiddy@gmail.com>
2015-04-11 14:05:07 -0700

    Menu Scene starts

---
+++ WizardDuel/Assets/Scripts/PlayerSlideScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSlideScript : MonoBehaviour {
+	public bool playing = false;
+	private float lerp = 0;
+	public float speed = 0.2f;
+	public int offset = 1;
+	private Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(playing && lerp < 1.0)
+		{
+			lerp += speed;
+		}
+		else if(!playing && lerp > 0)
+		{
+			lerp -= speed;
+		}
+		transform.position = new Vector3(startPos.x - offset*(1.0f-lerp),startPos.y,startPos.z);
+		                           
+	}
+	void setPlaying(bool playing)
+	{
+		playing = true;
+	}
+}
