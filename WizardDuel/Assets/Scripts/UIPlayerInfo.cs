19f146a406ef741cde746b3843ca36de9beb4066
Ethan Nakashima <ethanmn@hotmail.com>
2015-04-15 16:45:00 -0700

    Updated UI to only show players that are active in the game and count how many wins they have.

--- WizardDuel/Assets/Scripts/UIPlayerInfo.cs
+++ WizardDuel/Assets/Scripts/UIPlayerInfo.cs
@@ -7,11 +7,45 @@ public class UIPlayerInfo : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GameMonitorScript gm = GameObject.FindGameObjectWithTag("GameMonitor").GetComponent<GameMonitorScript>();
+		bool active = false;
+
+		foreach (PlayerInfo pi in gm.activePlayers)
+		{
+			if (player == pi.playerNum)
+			{
+				active = true;
+			}
+		}
+		/*if (player == "P1")
+		{
+			active = gameMonitor.GetComponent<GameMonitorScript>().p1Active;
+		}
+		else if (player == "P2")
+		{
+			active = gameMonitor.GetComponent<GameMonitorScript>().p2Active;
+		}
+		else if (player == "P3")
+		{
+			active = gameMonitor.GetComponent<GameMonitorScript>().p3Active;
+		}
+		else if (player == "P4")
+		{
+			active = gameMonitor.GetComponent<GameMonitorScript>().p4Active;
+		}*/
+		
+		if (active)
+		{
+			gameObject.GetComponent<CanvasGroup>().alpha = 1.0f;
+		}
+		else
+		{
+			gameObject.GetComponent<CanvasGroup>().alpha = 0.0f;
+		}
 	}
 }
---
+++ WizardDuel/Assets/WinCountScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class WinCountScript : MonoBehaviour {
+
+	private string player;
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponentInParent<UIPlayerInfo>().player;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameMonitorScript gm = GameObject.FindGameObjectWithTag("GameMonitor").GetComponent<GameMonitorScript>();
+		foreach (PlayerInfo pi in gm.activePlayers)
+		{
+			if (pi.playerNum == player)
+			{
+				gameObject.GetComponent<Text>().text = pi.wins.ToString();
+			}
+		}
+
+	}
+}
